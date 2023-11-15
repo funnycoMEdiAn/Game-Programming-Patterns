b16cef470a0cb629724d02d9bae57dcd21e410ce
Mea Rantala <dmera004@edu.xamk.fi>
2023-11-15 09:52:46 +0200

    Added spawner
    Added spawner and monster scripts

---
+++ Assets/Scripts/Monsters/Ghost.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : Monster
+{
+    private int health;
+    private int speed;
+
+    /*
+    public override Monster clone()
+    {
+        throw new System.NotImplementedException();
+    }*/
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
