ec13fe7837548f9c8e0516abe8fbfc0c448ede05
Mea Rantala <dmera004@edu.xamk.fi>
2023-11-22 08:59:15 +0200

    Updating Player states
    Updating player states

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,12 @@ public class PlayerController : MonoBehaviour
         CROUCHING
     }
 
+    public enum AbilityState
+    {
+        NONE,
+        SUPERSIZE
+    }
+
     public PlayerState my_state = PlayerState.JUMPING;
 
     Rigidbody _rigidbody;
@@ -69,8 +75,10 @@ public class PlayerController : MonoBehaviour
 
     public void OnCollisionEnter(Collision collision)
     {
-         my_state = PlayerState.STANDING;
-        Debug.Log(my_state);
+        if (my_state == PlayerState.JUMPING)
+        {
+            my_state = PlayerState.STANDING;
+        }
        // Lisää jotain schaisee jolla tunnistetaa et pelaaja osuu groundii ja sen state vaihtuu
     }
 
@@ -84,150 +92,116 @@ public class PlayerController : MonoBehaviour
 
         else
         {
-            if (Input.GetKeyDown(KeyCode.R))
+            switch (my_state)
             {
-                bReplaying = true;
-                while( _undo_commands.Count > 0)
+                case PlayerState.STANDING:
+
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        _rigidbody.AddForce(5.0f * transform.up, ForceMode.Impulse);
+                        my_state = PlayerState.JUMPING;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.W))
+                    {
+                        cmd_W.Execute(_rigidbody);
+                        _undo_commands.Push(cmd_W);
+                        _redo_commands.Clear();
+
+                        my_state = PlayerState.STANDING;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.S))
+                    {
+                        cmd_S.Execute(_rigidbody);
+                        _undo_commands.Push(cmd_S);
+                        _redo_commands.Clear();
+
+                        my_state = PlayerState.STANDING;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.A))
+                    {
+                        cmd_A.Execute(_rigidbody);
+                        _undo_commands.Push(cmd_A);
+                        _redo_commands.Clear();
+
+                        my_state = PlayerState.STANDING;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.D))
+                    {
+                        cmd_D.Execute(_rigidbody);
+                        _undo_commands.Push(cmd_D);
+                        _redo_commands.Clear();
+
+                        my_state = PlayerState.STANDING;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.C))
+                    {
+                        transform.localScale = new Vector3(1f, 0.5f, 1f);
+                        my_state = PlayerState.CROUCHING;
+                    }
+                    
+
+                    break;
+
+                case PlayerState.JUMPING:
+                    break;
+
+                case PlayerState.CROUCHING:
+                    if (Input.GetKeyDown(KeyCode.C))
+                    {
+                        transform.localScale = new Vector3(1f, 1f, 1f);
+                        my_state = PlayerState.STANDING;
+                    }
+                    break;
+
+            }
+
+                if (Input.GetKeyDown(KeyCode.R))
                 {
-                    _replay_commands.Push(_undo_commands.Pop());
-                }
-                transform.position = start_pos;
+                    bReplaying = true;
+                    while (_undo_commands.Count > 0)
+                    {
+                        _replay_commands.Push(_undo_commands.Pop());
+                        
+                    }
+                    transform.position = start_pos;
 
-                StartCoroutine(Replay());
-
-            }
-
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                cmd_W.Execute(_rigidbody);
-                _undo_commands.Push(cmd_W);
-                _redo_commands.Clear();
-
-                my_state = PlayerState.STANDING;
-                Debug.Log(my_state);
-
-                //_last_command = cmd_W;
-                //transform.position += Vector3.forward;
-            }
-
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                cmd_S.Execute(_rigidbody);
-                _undo_commands.Push(cmd_S);
-                _redo_commands.Clear();
-
-                my_state = PlayerState.STANDING;
-                Debug.Log(my_state);
-
-                //_last_command = cmd_S;
-                //transform.position += Vector3.back;
-            }
-
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                cmd_A.Execute(_rigidbody);
-                _undo_commands.Push(cmd_A);
-                _redo_commands.Clear();
-
-                my_state = PlayerState.STANDING;
-                Debug.Log(my_state);
-
-                //_last_command = cmd_A;
-                //transform.position += Vector3.left;
-            }
-
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                cmd_D.Execute(_rigidbody);
-                _undo_commands.Push(cmd_D);
-                _redo_commands.Clear();
-
-                my_state = PlayerState.STANDING;
-                Debug.Log(my_state);
-
-                //_last_command = cmd_D;
-                //transform.position += Vector3.right;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _rigidbody.AddForce(5.0f * transform.up, ForceMode.Impulse);
-                my_state = PlayerState.JUMPING;
-                Debug.Log(my_state);
-            }
-
-
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                if (_undo_commands.Count > 0)
-                {
-                    transform.localScale = new Vector3(1f, 0.5f, 1f);
-
-                    my_state = PlayerState.CROUCHING;
-                    Debug.Log(my_state);
-                }
-
-                else
-                {
-                    transform.localScale = new Vector3(1f, 1f, 1f);
-                }
-
-            }
-
-            if (Input.GetKeyDown(KeyCode.V)) //Typerä tapa päästä pois chroucista
-            {
-                if (_undo_commands.Count > 0)
-                {
-                    transform.localScale = new Vector3(1f, 1f, 1f);
-
+                    StartCoroutine(Replay());
+                    //my_state = PlayerState.REPLAYING;
                     my_state = PlayerState.STANDING;
-                    Debug.Log(my_state);
+
                 }
 
-            }
-
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                if (_undo_commands.Count > 0)
+                if (Input.GetKeyDown(KeyCode.Z))
                 {
-                    Command cmd = _undo_commands.Pop();
-                    _redo_commands.Push(cmd);
-                    cmd.Undo(_rigidbody);
+                    if (_undo_commands.Count > 0)
+                    {
+                        Command cmd = _undo_commands.Pop();
+                        _redo_commands.Push(cmd);
+                        cmd.Undo(_rigidbody);
 
-                    my_state = PlayerState.STANDING;
-                    Debug.Log(my_state);
-
-                    //_undo_commands.Pop().Undo(_rigidbody);
+                        my_state = PlayerState.STANDING;
+                    }
                 }
 
-                //_last_command = new DoNothingCommand();
-            }
-
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-                if (_redo_commands.Count > 0)
+                if (Input.GetKeyDown(KeyCode.X))
                 {
-                    Command cmd = _redo_commands.Pop();
-                    _undo_commands.Push(cmd);
-                    cmd.Execute(_rigidbody);
+                    if (_redo_commands.Count > 0)
+                    {
+                        Command cmd = _redo_commands.Pop();
+                        _undo_commands.Push(cmd);
+                        cmd.Execute(_rigidbody);
 
-                    my_state = PlayerState.STANDING;
-                    Debug.Log(my_state);
+                        my_state = PlayerState.STANDING;
+
+                    }
 
                 }
 
-                /*  //Omat vanhat koodit
-                _last_command.Redo(_rigidbody);
-                _last_command = new DoNothingCommand();
-                Debug.Log("R pressed");
-                */
-            }
-
-            /* //Jos haluaa vaihtaa jotkin napit toisin päin
-            if (Input.GetKeyDown(KeyCode.Escape)){
-                SwapCommands(ref cmd_A, ref cmd_D);
-            }
-            */
         }
 
     }
